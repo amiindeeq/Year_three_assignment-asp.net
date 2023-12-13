880a8570586edf904033c26a46a4001501c640ae
amiindeeq <mdeeqxussen@gmail.com>
2023-12-13 19:28:23 +0300

    first commit

---
+++ Controllers/studController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using Year_three_assignment.Models;
+
+namespace Year_three_assignment.Controllers
+{
+    public class studController : Controller
+    {
+       public IActionResult assignment()
+        {
+            return View();
+        }
+        public IActionResult lists()
+        {
+            List<student> modalData = new List<student>();
+
+            student obj1 = new student();
+            obj1.Id = 1;
+            obj1.Name = "Mohammed Deik Hussein";
+            obj1.Mobile = "634117825";
+            obj1.Course = "Asp.net";
+            modalData.Add(obj1);
+
+            student obj2 = new student();
+            obj2.Id = 2;
+            obj2.Name = "Mohammed Ahmed";
+            obj2.Mobile = "634103837";
+            obj2.Course = "Java";
+            modalData.Add(obj2);
+
+            
+            // Add more student objects as needed
+
+            return View(modalData);
+        }
+        public IActionResult Registration()
+        {
+            student emptymodel = new student();
+            return View(emptymodel);
+        }
+        [HttpPost]
+        public IActionResult Create(student model)
+        {
+            return View(model);
+        }
+    }
+}
---
+++ Models/student.cs
@@ -0,0 +1,10 @@
+﻿namespace Year_three_assignment.Models
+{
+    public class student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Mobile { get; set; }
+        public string Course { get; set; }
+    }
+}
